3053e9dcadb88b4a85e7767e2ff7a7dbc5cf2763
Brandon Altvater <bdaltvater@gmail.com>
2018-05-02 23:18:06 -0400

    initial commit

---
+++ src/main/java/starwars/controller/StarWarsController.java
@@ -0,0 +1,51 @@
+package starwars.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import starwars.exceptions.RestException;
+import starwars.mapper.StarWarsMapper;
+import starwars.objects.Characteristics;
+
+@RestController
+@RequestMapping("/starwars")
+public class StarWarsController {
+
+	private static final String DARTH_VADER = "darthvader";
+	private static final String LEAH = "leia";
+	private static final String LUKE = "luke";
+
+	@RequestMapping(value = "/character/{name}")
+	public Characteristics getCharacteristics(@PathVariable(value = "name") String name) {
+
+		Characteristics characteristics;
+		characteristics = getValues(name);
+		return characteristics;
+	}
+
+	private Characteristics getValues(String name) {
+
+		Characteristics characteristics = new Characteristics();
+
+		switch (name) {
+			case DARTH_VADER:
+				characteristics.setName("Anakin Skywalker");
+				characteristics.setHeight("6 ft 2 in");
+				characteristics.setWeight("196 lbs");
+				return characteristics;
+			case LEAH:
+				characteristics.setName("Princess Leia");
+				characteristics.setHeight("5 ft 1 in");
+				characteristics.setWeight("126 lbs");
+				return characteristics;
+			case LUKE:
+				characteristics.setName("Luke Skywalker");
+				characteristics.setHeight("5 ft 6 in");
+				characteristics.setWeight("161 lbs");
+				return characteristics;
+			default:
+				throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown character requested");
+		}
+	}
+}
---
+++ src/main/java/starwars/objects/Characteristics.java
@@ -0,0 +1,32 @@
+package starwars.objects;
+
+public class Characteristics {
+
+	private String name;
+	private String height;
+	private String weight;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+}
